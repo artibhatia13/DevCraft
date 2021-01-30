593b0430923656436ba5f0c20957703c9c989c25
artibhatia13 <bhatiaarti138@gmail.com>
2021-01-30 17:31:10 +0530

    .

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import landing from '../assests/landing.jpg'
 
 const Home = () => {
   return (
-    <div className="home" style={{ backgroundColor: "grey", height: "90vh" }}>
+    <div className="home white" style={{ backgroundColor: "grey", height: "90vh" }}>
       <div style={{ display: "flex" }}>
-        <div>
+        <div className="landing white z-depth-4">
           <p>Your one stop destination for all things fun and educational</p>
-          <Link to="/login" className="btn">
+          <br/>
+          <Link to="/login" className="btn z-depth-0">
             Get Started
           </Link>
         </div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS21VMvVVdfB_E_q7KMxkIDT1_CxXWx3shonQ&usqp=CAU"
-          alt=""
-        />
+        <img src={landing} style={{margin:'8em 0 0 0',height:'19em',width:'auto'}}/>
       </div>
     </div>
   );
